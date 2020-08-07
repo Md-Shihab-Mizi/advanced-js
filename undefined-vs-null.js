92d84b78b95962187b895b28fa6cb38e8e0124f3
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-08-07 12:27:06 +0600

    null vs undefined cleared for interview

---
+++ undefined-vs-null.js
@@ -0,0 +1,43 @@
+// how get I undefined rule Number 1
+// let pakhi;
+// console.log(pakhi);
+
+
+// how get I undefined rule Number 2
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+// const result = add(20, 50);
+// console.log(result);
+
+
+// how get I undefined rule Number 3
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(20, 50);
+// console.log(result);
+
+
+// how get I undefined rule Number 4
+// function add(num1, num2){
+//     console.log(num1 , num2);
+
+// }
+// const result = add(20);
+// console.log(result);
+
+// how get I undefined rule Number 5
+// const person = { name: "shihab", phone:325432};
+// console.log(person.age);
+
+
+// how get I undefined rule Number 6
+// let fun = undefined;
+// console.log(fun);
+
+
+// how get I undefined rule Number 7
+let ages = [2, 5, 6];
+console.log(ages[11])
