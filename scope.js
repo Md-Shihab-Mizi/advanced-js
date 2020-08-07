7259c41b39583f6495d82abd5d68eb45a57f208e
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-08-07 16:17:43 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+const bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        const mood = 'happy'
+        console.log(mood);
+    }
+    console.log(mood);
+// console.log(day)
+//     var day = 'friday';
+ 
+    return result;
+}
+const output = sum(3, 7);
+// console.log(result);
+// console.log(bonus);
+// console.log(output);
